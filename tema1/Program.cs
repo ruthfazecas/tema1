e0c2275c6640906f0baaa4a2e9f4e52e25cad2cb
Ruth Fazecas <ruth.fazecas@emanuel.ro>
2021-03-03 20:08:52 +0200

    initial commit

---
+++ tema1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
